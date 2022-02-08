ffcaaefef3cad3e09b22335e654ed129668ce18a
Antoine Leblanc <intoine@hotmail.com>
2022-02-08 16:36:46 -0500

    correction rapport paquet et toutes les mesures pour avoir chemin récursif

--- src/Code/WMC.java
+++ src/Code/WMC.java
@@ -5,23 +5,21 @@ import java.util.ArrayList;
 
 public class WMC {
     /**
-     *
      * @param file classes actuel
      * @return le nombre de McCabe
      */
-    public static int getWMCClass(File file){
-        int res=0;
-        ArrayList<String> listString=CLOC.getClassString(file);
+    public static int getWMCClass(File file) {
+        int res = 0;
+        ArrayList<String> listString = CLOC.getClassString(file);
         //pour chaque ligne, regarde si ouverture de bracket
-        for(String row :listString){
-            if(row.contains("){")){
-                res+=1;
-            }
-            else if(row.contains("if")){
-                res+=1;
+        for (String row : listString) {
+            if (row.contains("){")) {
+                res += 1;
+            } else if (row.contains("if")) {
+                res += 1;
             }
             //pour le do while
-            else if(row.contains("while")){
+            else if (row.contains("while")) {
                 res++;
             }
 
@@ -30,39 +28,46 @@ public class WMC {
     }
 
     /**
-     *
      * @param folder package actuel
      * @return le nombre de WCP
      */
-    public  static  int getWMCPackage(File folder){
-        int res=0;
+    public static int getWMCPackage(File folder) {
+        int res = 0;//si nest pas un répertoire
+        if (!folder.isDirectory()) {
+            res += getWMCClass(folder);
+        }
         //https://stackoverflow.com/questions/1844688/how-to-read-all-files-in-a-folder-from-java
         //pour chaque fichier du paquet
-        for (final File fileEntry : folder.listFiles()) {
-            //si nest pas un répertoire
-            if (!fileEntry.isDirectory()) {
-                res+=getWMCClass(fileEntry);
+        else {
+            for (final File fileEntry : folder.listFiles()) {
+                res += getWMCPackage(fileEntry);
             }
         }
         return res;
     }
 
     /**
-     *
      * @param file fichier actuel
      * @return le BC de la classe
      */
-    public static  double getClassBC(File file){
-        return CLOC.getDensityClass(file) /(double)getWMCClass(file);
+    public static double getClassBC(File file) {
+        double wmc=getWMCClass(file);
+        if(wmc==0.0){
+            return 0;
+        }
+        return CLOC.getDensityClass(file) / wmc;
     }
 
     /**
-     *
      * @param folder répertoire du paquet
      * @return BC du paquet
      */
-    public static  double getPackageBC(File folder){
-        return CLOC.getDensityPackage(folder) /(double)getWMCPackage(folder);
+    public static double getPackageBC(File folder) {
+        double wcp=getWMCPackage(folder);
+        if(wcp==0.0){
+            return 0;
+        }
+        return CLOC.getDensityPackage(folder) / wcp;
     }
 
 
